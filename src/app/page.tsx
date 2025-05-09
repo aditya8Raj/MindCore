e3c4d402d7e7a9e92bd6e5ff4650d7b7b1f4c774
Aditya Raj <aditya7raj1@gmail.com>
2025-05-09 10:42:15 +0530

    useEffect error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { Hero } from "@/components/homePage/Hero";
 import { Loader } from "@/components/ui/Loader";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
